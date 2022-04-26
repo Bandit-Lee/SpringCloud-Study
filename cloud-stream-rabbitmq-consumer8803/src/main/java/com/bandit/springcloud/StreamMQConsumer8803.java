61827232f734517c1b2b6a49935f448856cf6e3a
bandit <2577569090@qq.com>
2022-04-26 19:10:50 +0800

    Spring-Cloud-Stream: 强迫症还是学了，2版本的Stream-rabbit

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/bandit/springcloud/StreamMQConsumer8803.java
@@ -0,0 +1,17 @@
+package com.bandit.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Bandit
+ * @createTime 2022/4/26 17:39
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQConsumer8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQConsumer8803.class, args);
+    }
+}
